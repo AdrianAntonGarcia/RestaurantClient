1c2f33411d3764f04007971bf6e3cd335b208fb2
adriton <adrtler@gmail.com>
2021-04-17 18:42:33 +0200

    Finalizando el sidebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
+
 import Ordenes from './components/paginas/Ordenes';
 import Menu from './components/paginas/Menu';
 import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+import Sidebar from './components/ui/Sidebar';
 
 export const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Ordenes />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
-      </Routes>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NuevoPlatillo = () => {
-  return <div></div>;
+  return <div>Nuevo Platillo</div>;
 };
 
 export default NuevoPlatillo;
