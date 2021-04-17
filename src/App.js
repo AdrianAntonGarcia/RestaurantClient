475f8916b3524f28ddd30a8486a593ae192e7325
adriton <adrtler@gmail.com>
2021-04-17 18:23:40 +0200

    Creando rutas y componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router';
 import Ordenes from './components/paginas/Ordenes';
+import Menu from './components/paginas/Menu';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Ordenes />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
       </Routes>
     </div>
   );
---
+++ src/components/paginas/NuevoPlatillo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NuevoPlatillo = () => {
+  return <div></div>;
+};
+
+export default NuevoPlatillo;
