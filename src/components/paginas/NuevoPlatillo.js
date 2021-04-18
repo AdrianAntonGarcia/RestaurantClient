a0aa37b05b7049a77eae3190b88c529be7ecd3db
adriton <adrtler@gmail.com>
2021-04-18 10:52:09 +0200

    Mostrando porcentaje de subida

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -184,6 +184,21 @@ const NuevoPlatillo = () => {
                 onProgress={handleProgress}
               />
             </div>
+            {subiendo && (
+              <div className="h-12 w-full border">
+                <div
+                  className="bg-green-500 absolute left-0 top-0 text-white px-2 text-sm h-12 flex items-center"
+                  style={{ width: `${progreso}%` }}
+                >
+                  {progreso} %
+                </div>
+              </div>
+            )}
+            {urlImagen && (
+              <p className="bg-green-500 text-white p-3 text-center my-5">
+                La imagen se subió correctamente
+              </p>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
